a1e33aae416b10bab3820709182b62b57e3e8030
kn5 <ognekian@gmail.com>
2017-12-14 03:49:51 +0900

    remove dot

--- setup.sh
+++ setup.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-DOT_FILES=(.bashrc .vimshrc .bash_profile .screenrc .nvimrc config.fish .tmux.conf .latexmkrc)
+DOT_FILES=(bashrc vimshrc bash_profile screenrc nvimrc tmux.conf latexmkrc)
 
 for file in ${DOT_FILES[@]}
 do
-  ln -is $HOME/dotf/$file $HOME/$file
+  ln -is $HOME/dotf/$file $HOME/.$file
 done
+
+mkdir -p $HOME/.config/fish
+ln -is $HOME/dotf/config.fish $HOME/.config/fish/config.fish
 
 mkdir -p $HOME/.config/nvim
 mkdir -p $HOME/.vim
-ln -is $HOME/dotf/.nvimrc $HOME/.config/nvim/init.vim
+ln -is $HOME/dotf/nvimrc $HOME/.config/nvim/init.vim
 if [ ! -d $HOME/.config/nvim/rc ]; then
   ln -s $HOME/dotf/rc $HOME/.config/nvim/rc
 fi
 if [ ! -d $HOME/.vim/rc ]; then
   ln -s $HOME/dotf/rc $HOME/.vim/rc
 fi
-ln -is $HOME/dotf/.nvimrc $HOME/.vimrc
+ln -is $HOME/dotf/nvimrc $HOME/.vimrc
